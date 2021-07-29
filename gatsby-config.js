ddfe75f585d4321690f8ad2adee56a4c2a12db04
Tushar <tusharxdev@gmail.com>
2021-07-29 10:25:34 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "frontend-dev-test",
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"],
+};
